aa35962f6bc140c751e0298998ea5a540c615f0d
Bibi <84073518+xBibie@users.noreply.github.com>
2021-07-02 13:50:59 +0200

    Add files via upload

---
+++ function.js
@@ -0,0 +1,10 @@
+const sqaured = function (number1, number2){
+    Math.sqrt(number1);
+    Math.sqrt(number2);
+    add (number1,number2);
+    Math.sqrt(result);
+};
+console.log (result);
+const squared = (5, 6)
+
+
